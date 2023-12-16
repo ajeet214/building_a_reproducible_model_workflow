eca41ed4f750cfb8fe3651660adb15d95d62bf5f
Ajeet <ajeet214.90@gmail.com>
2023-12-16 22:24:19 +0700

    code refactoring

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -5,6 +5,12 @@ import scipy.stats
 
 
 def test_column_names(data):
+    """
+    Test whether the columns in the dataset match the expected column names.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
+    """
 
     expected_colums = [
         "id",
@@ -25,13 +31,18 @@ def test_column_names(data):
         "availability_365",
     ]
 
-    these_columns = data.columns.values
-
-    # This also enforces the same order
-    assert list(expected_colums) == list(these_columns)
+    # Enforce the same order
+    assert list(expected_columns) == list(data.columns.values)
 
 
 def test_neighborhood_names(data):
+
+    """
+    Test whether the 'neighbourhood_group' column contains known neighborhood names.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
+    """
 
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
 
@@ -43,8 +54,12 @@ def test_neighborhood_names(data):
 
 def test_proper_boundaries(data: pd.DataFrame):
     """
-    Test proper longitude and latitude boundaries for properties in and around NYC
+    Test proper longitude and latitude boundaries for properties in and around NYC.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
     """
+
     idx = data['longitude'].between(-74.25, -
                                     73.50) & data['latitude'].between(40.5, 41.2)
 
@@ -57,8 +72,14 @@ def test_similar_neigh_distrib(
         kl_threshold: float):
     """
     Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
+    significantly different than that of the reference dataset.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
+        ref_data (pd.DataFrame): The reference dataset for comparison.
+        kl_threshold (float): The threshold for the KL divergence.
     """
+
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
 
@@ -67,13 +88,23 @@ def test_similar_neigh_distrib(
 
 def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
     """
-    Test that the price range is within the expected boundaries
+    Test that the price range is within the expected boundaries.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
+        min_price (float): The minimum expected price.
+        max_price (float): The maximum expected price.
     """
+
     assert data['price'].between(min_price, max_price).all()
 
 
 def test_row_count(data):
     """
-    Test that the number of rows in the dataset is within the expected boundaries
+    Test that the number of rows in the dataset is within the expected boundaries.
+
+    Args:
+        data (pd.DataFrame): The dataset to be tested.
     """
-    assert data.shape[0] > 15000 and data.shape[0] < 100000
+
+    assert 15000 < data.shape[0] < 100000
